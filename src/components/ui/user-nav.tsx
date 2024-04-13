eab5986574752d73b4cd7fe27135e45080163f01
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-13 18:44:55 +0200

    user nav modified

--- src/components/ui/user-nav.tsx
+++ src/components/ui/user-nav.tsx
@@ -29,7 +29,7 @@ export function UserNav() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">Mike Doen</p>
             <p className="text-xs leading-none text-muted-foreground">
               m@example.com
             </p>
