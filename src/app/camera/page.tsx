5e498e8388edf9da7facf152f54c9a0e3da1e077
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-15 16:10:24 +0200

    map-card made

---
+++ src/app/camera/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CameraPage = () => {
+  return (
+      <div className='container'>
+          CameraPage
+
+          <div>
+              <div className="w-100"><iframe width="100%" height="600"  scrolling="no" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Lake%20Kivu,%20Blarirwa+(EcoWatch%20)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
+          </div>
+      </div>
+  )
+}
+
+export default CameraPage;
