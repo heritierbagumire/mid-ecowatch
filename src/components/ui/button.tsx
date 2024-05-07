6b334e548f2d2c5133a125b47a019ead5a1c732c
heritierbagumire <heritierbagumire367@gmail.com>
2024-05-07 13:53:52 +0200

    button changed abit

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -33,8 +32,8 @@ const buttonVariants = cva(
     },
   }
 )
+
 export interface ButtonProps
-  
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
